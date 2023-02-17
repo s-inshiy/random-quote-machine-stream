33446975526f1233beb77bcb12dd9fc0b9832f1c
Serhii Slesarev <slesarev071290@gmail.com>
2023-02-17 16:19:10 +0200

    feat: add getRandomColor function

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,12 @@
 export function getRandomNumber(number) {
   return Math.floor(Math.random() * number);
 }
+
+export function getRandomColor() {
+  const color = Math.floor(
+    Math.random() * 16777215
+  )
+    .toString(16)
+    .padStart(6, '0');
+  return `#${color}`;
+}
